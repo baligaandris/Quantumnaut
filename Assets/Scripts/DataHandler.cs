211b92b90bdc5f49b76136b6f40cc68f5798a159
preslavb <preslavb@abv.bg>
2017-04-22 19:51:41 +0100

    Final Player Commit

---
+++ Assets/Scripts/DataHandler.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public static class DataHandler
+{
+    public static int numberOfTilesInLevel = 7;
+    public static GameObject[,] tilesInLevel = new GameObject[numberOfTilesInLevel, numberOfTilesInLevel];
+    public static PlayerScript player;
+}
+
